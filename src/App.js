f160f4a4b06070a2c2ce2ce95fa9e16f5773a470
Repchan <96149619+Repchan@users.noreply.github.com>
2023-12-09 16:43:25 +0200

    Final

---
+++ src/App.js
@@ -0,0 +1,26 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+import React, { useEffect } from 'react';
+import { RouterProvider } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
+import router from './routers/router';
+import { isAccessTokenExpire } from './utils/helpers';
+import { useAuth } from './hooks/useAuth';
+import { logout } from './redux/slices/authSlice';
+
+const App = () => {
+  const { expire } = useAuth();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (isAccessTokenExpire(expire)) {
+      dispatch(logout());
+    }
+  }, [expire, dispatch]);
+
+  return <RouterProvider router={router} />;
+};
+
+export default App;
